7d77f5fdffa158fc9f584e2a0ad53b03defd633b
Kartik Nagre <kanagre@syr.edu>
2018-11-18 11:02:01 -0500

    change in directory structure

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import contact
 import pandas as pd
 import math
 import requests
 import json
+import sys
+from lib.contact import Contact
+from lib.constants import *
+
+
+
+
+print(sys.argv)
+
+
+
+
 
 
 
