548c58f4655d6fd3645667a2bd628861fc2d2a5e
Kartik Nagre <kanagre@syr.edu>
2018-11-18 11:50:23 -0500

    added methods to generate attributes payload

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ class MissingContacts(object):
         self.contacts_file = contacts_file
         self.sector_name_list = required_sectors
         self.df_codes = pd.read_excel(self.codes_file)
+        self.df_contacts = pd.read_csv(self.contacts_file)
 
 
     def get_sector_codes(self, sector_name):
@@ -25,7 +26,6 @@ class MissingContacts(object):
         required_sector_codes = []
         for sector in sector_name_list:
             sector = sector.rstrip(',')
-            print('=====SECTOR ===== ', sector)
             sector_code_list.append(str(self.get_sector_codes(sector)))
         for code in sector_code_list:
             if '-' in code:
@@ -36,12 +36,47 @@ class MissingContacts(object):
         return required_sector_codes
 
 
+    def get_sector_name(self, code):
+        return self.df_codes[self.df_codes['2017 NAICS US   Code'] == code]['2017 NAICS US Title'].values[0]
+
+
+    def get_attributes_payload(self, contact):
+        return {'first_name': contact['first_name'],
+                'last_name': contact['last_name'],
+                'email': contact['email'],
+                'company_name': contact['company_name'],
+                'industry_name': self.get_sector_name(contact['naics_sector'])}
+
+
+    def start_post_contacts(self, required_sector_codes):
+        df_10 = self.df_contacts.head(10)
+        for index, row in df_10.iterrows():
+            contacts_sector = str(row['naics_sector'])
+            if contacts_sector.startswith(required_sector_codes[0]) or contacts_sector.startswith(
+                    required_sector_codes[1]) or contacts_sector.startswith(
+                required_sector_codes[2]) or contacts_sector.startswith(required_sector_codes[3]):
+                self.create_attributes_relationships(row)
+
+
+    def create_attributes_relationships(self, contact):
+        attributes_payload = self.get_attributes_payload(contact)
+        print(attributes_payload)
+        # relationships_payload = get_relationships_payload(contact)
+        # print(relationships_payload)
+        # print(' ------ ----- ------')
+        # contact_object = Contact(attributes_payload, relationships_payload)
+        # post_contact_payload(contact_object)
+
+
 if __name__ == "__main__":
     sys.argv.pop(0)
     required_sectors = sys.argv
     print(required_sectors)
     mc = MissingContacts(CODES_FILE, CONTACTS_FILE, required_sectors)
-    print(mc.get_required_sector_codes(required_sectors))
+    required_sector_codes = mc.get_required_sector_codes(required_sectors)
+    mc.start_post_contacts(required_sector_codes)
+
+
 
 
 
