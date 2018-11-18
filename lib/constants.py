f1a117ce012edcef584ec54f901cc25af199c1cb
Kartik Nagre <kanagre@syr.edu>
2018-11-18 15:16:37 -0500

    added more urls, headers as constants

--- lib/constants.py
+++ lib/constants.py
@@ -1,2 +1,9 @@
-CODES_FILE = './ingestion_files/2-6 digit_2017_Codes.xlsx'
-CONTACTS_FILE = './ingestion_files/contacts.csv'
+class Constants(object):
+    CODES_FILE = './ingestion_files/2-6 digit_2017_Codes.xlsx'
+    CONTACTS_FILE = './ingestion_files/contacts.csv'
+    CONTACT_EMAIL_URL = 'https://dataengineerinterview.thunderturtle.io/contacts/search?email='
+    CONTACT_URL_API_HEADER_KEY = 'X-Api-Key'
+    CONTACT_URL_API_HEADER_VALUE = 'yjciUwPfqW3IfQZaZRQlc4vrPjrPjs026cE67uF0'
+    CONTACT_POST_URL = 'https://dataengineerinterview.thunderturtle.io/contacts'
+    CONTENT_TYPE_HEADER_KEY = 'Content-Type'
+    CONTENT_TYPE_JSON_VALUE = 'application/json'
